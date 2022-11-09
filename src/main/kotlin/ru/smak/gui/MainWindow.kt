525fb2fa4422dc40e98f89bce0c5a1b9abe23a46
Sergey Makletsov <smaklets@kpfu.ru>
2022-11-09 16:42:04 +0300

    Рисование рамки области выделения вниз, вправо

--- src/main/kotlin/ru/smak/gui/MainWindow.kt
+++ src/main/kotlin/ru/smak/gui/MainWindow.kt
@@ -3,18 +3,21 @@ package ru.smak.gui
 import ru.smak.graphics.FractalPainter
 import ru.smak.graphics.Plane
 import ru.smak.graphics.testFunc
+import ru.smak.main
 import ru.smak.math.Mandelbrot
 import java.awt.Color
 import java.awt.Dimension
+import java.awt.Point
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import java.util.GregorianCalendar
 import javax.swing.GroupLayout
 import javax.swing.JFrame
 
 class MainWindow : JFrame() {
+    private var firstPoint: Point? = null
+    private var prevPoint: Point? = null
     val minSz = Dimension(600, 450)
     val mainPanel: GraphicsPanel
     init {
@@ -38,16 +41,48 @@ class MainWindow : JFrame() {
         mainPanel.addMouseListener(object : MouseAdapter(){
             override fun mousePressed(e: MouseEvent?) {
                 super.mousePressed(e)
+                e?.let {
+                    firstPoint = it.point
+                }
             }
-
             override fun mouseReleased(e: MouseEvent?) {
                 super.mouseReleased(e)
+                firstPoint?.let {first->
+                    val g = mainPanel.graphics
+                    g.color = Color.BLACK
+                    g.setXORMode(Color.WHITE)
+                    prevPoint?.let { prev ->
+                        val psW = prev.x - first.x
+                        val psH = prev.y - first.y
+                        g.drawRect(first.x, first.y, psW, psH)
+                    }
+                    g.setPaintMode()
+                }
+                firstPoint = null
+                prevPoint = null
             }
         })
 
         mainPanel.addMouseMotionListener(object : MouseAdapter(){
             override fun mouseDragged(e: MouseEvent?) {
                 super.mouseDragged(e)
+                e?.let{ curr->
+                    firstPoint?.let { first ->
+                        val sW = curr.x - first.x
+                        val sH = curr.y - first.y
+                        val g = mainPanel.graphics
+                        g.color = Color.BLACK
+                        g.setXORMode(Color.WHITE)
+                        prevPoint?.let {prev ->
+                            val psW = prev.x - first.x
+                            val psH = prev.y - first.y
+                            g.drawRect(first.x, first.y, psW, psH)
+                        }
+                        g.drawRect(first.x, first.y, sW, sH)
+                        g.setPaintMode()
+                        prevPoint = curr.point
+                    }
+                }
             }
         })
 
@@ -63,6 +98,15 @@ class MainWindow : JFrame() {
                 .addComponent(mainPanel, GROW, GROW, GROW)
                 .addGap(8)
             )
+        }
+    }
+
+    override fun setVisible(b: Boolean) {
+        super.setVisible(b)
+        mainPanel.graphics.run{
+            setXORMode(Color.WHITE)
+            drawLine(-100, -100, -101, -100)
+            setPaintMode()
         }
     }
 
